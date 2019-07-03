bda8f2b76e1f9f5bd3f70526828b9227f9f7a9bb
Sercan Solmaz <code@srcnslmz.com>
2019-07-03 10:42:48 +0300

    Create initial snake.

--- snake.js
+++ snake.js
@@ -1,11 +1,14 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const context = gameCanvas.getContext("2d");
 
+var snake = [ {x: 250, y: 250}, {x: 240, y: 250}, {x: 230, y: 250}, {x: 220, y: 250} ]
+
 goSnake();
 
 function goSnake() {
     createCanvas();
     generateFood();
+    generateSnake();
 }
 
 function createCanvas() {
@@ -25,4 +28,16 @@ function generateFood() {
 
     context.fillRect(foodX, foodY, 10, 10);
     context.strokeRect(foodX, foodY, 10, 10);
+}
+
+function generateSnake() {
+    snake.forEach(drawSnake)
+}
+
+function drawSnake(snakePart) {
+    context.fillStyle = "green";
+    context.strokestyle = "black";
+
+    context.fillRect(snakePart.x, snakePart.y, 10, 10);
+    context.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
