9ad8b6f9f48b33b6ca70c18e83a4abb858de35d7
Sercan Solmaz <code@srcnslmz.com>
2019-07-03 09:30:45 +0300

    Create food.

--- snake.js
+++ snake.js
@@ -1,15 +1,28 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const context = gameCanvas.getContext("2d");
-        
+
 goSnake();
 
 function goSnake() {
     createCanvas();
+    generateFood();
 }
 
 function createCanvas() {
     context.fillStyle = "white";
     context.strokestyle = "black";
+
     context.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
     context.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+
+function generateFood() {
+    context.fillStyle = "red";
+    context.strokestyle = "black";
+
+    var foodX = (Math.round((Math.random() * (gameCanvas.width - 10) / 10) * 10));
+    var foodY = (Math.round((Math.random() * (gameCanvas.height - 10) / 10) * 10));
+
+    context.fillRect(foodX, foodY, 10, 10);
+    context.strokeRect(foodX, foodY, 10, 10);
 }
